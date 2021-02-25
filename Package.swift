236c9a850b790d0e60664e656810f5c4c34d04aa
Andy Liu <andy@madmachine.io>
2021-02-25 20:06:48 +0800

    Test
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Board",
-            dependencies: []),
+            dependencies: [
+                "SwiftIOBoard",
+                "SwiftIOFeather"
+            ]),
         .target(
             name: "SwiftIOBoard",
             dependencies: ["SwiftIO"]),
