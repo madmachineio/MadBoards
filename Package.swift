4df3357e9e202a608649682e7e2f08caad697a8a
Andy Liu <andy@madmachine.io>
2021-02-25 19:45:43 +0800

    Inital push
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -8,8 +8,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Board",
-            targets: ["SwiftIOBoard", "SwiftIOFeather"]),
+            name: "SwiftIOBoard",
+            targets: ["SwiftIOBoard"]),
+        .library(
+            name: "SwiftIOFeather",
+            targets: ["SwiftIOFeather"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
