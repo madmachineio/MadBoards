2ccb704137cb612201f5549c3b7b3ad729ba6b3e
Andy Liu <andy@madmachine.io>
2021-03-01 14:08:08 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Board",
+    name: "MadBoards",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Board",
+            name: "MadBoards",
             targets: ["SwiftIOBoard", "SwiftIOFeather"]),
     ],
     dependencies: [
