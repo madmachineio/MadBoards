1df888b27f9f380b9fd5a3390f9c0ccaa4d57203
Andy Liu <andy@madmachine.io>
2021-03-18 16:07:47 +0800

    Update
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Sources/SwiftIOFeather/Id.swift
+++ Sources/SwiftIOFeather/Id.swift
@@ -5,11 +5,4 @@ public enum Id: Int32, IdName {
         self.rawValue & Int32(0xFF)
     }
 
-    case D0 = 0x0000, D1
-    case A0 = 0x0100, A1
-    case PWM0A = 0x0200
-    case I2C0 = 0x0300
-    case SPI0 = 0x0400
-    case UART0 = 0x0500
-    case C0 = 0x0600
 }
