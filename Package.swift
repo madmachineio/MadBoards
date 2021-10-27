dd068d00ca1846950e9b312f452cbf360bfa318c
Andy Liu <andy@madmachine.io>
2021-10-26 19:44:26 -0700

    Merge boards (#1)
    * Merge all boards into one target: MadBoard, use predefined compiling
    
    Signed-off-by: Andy Liu <andy@madmachine.io>
    
    * Add license
    
    * Create README.md
    
    Co-authored-by: Ines Zhou <ines@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -9,16 +9,20 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MadBoards",
-            targets: ["SwiftIOBoard", "SwiftIOFeather"]),
+            targets: ["MadBoard", "SwiftIOBoard", "SwiftIOFeather"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", from: "0.0.1"),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", .upToNextMajor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MadBoard",
+            dependencies: ["SwiftIO"],
+            path: "./Sources/MadBoards"),
         .target(
             name: "SwiftIOBoard",
             dependencies: ["SwiftIO"]),
