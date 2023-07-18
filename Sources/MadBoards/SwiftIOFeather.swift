46e6c53ddfd410333c7abfc95979bb33c27a7e97
Andy Liu <andy@madmachine.io>
2023-07-17 19:34:27 -0700

    Feature/feather v3 (#4)
    * Modify dependency branch
    
    Signed-off-by: Andy Liu <andy@madmachine.io>
    
    * Modify Feather Pin ID
    
    Signed-off-by: Andy Liu <andy@madmachine.io>
    
    * Add feather_v3 branch
    
    Signed-off-by: Andy Liu <andy@madmachine.io>
    
    * Modify I2S Id
    
    Signed-off-by: Andy Liu <andy@madmachine.io>
    
    ---------
    
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Sources/MadBoards/SwiftIOFeather.swift
+++ Sources/MadBoards/SwiftIOFeather.swift
@@ -5,7 +5,7 @@
 //
 // Authors: Andy Liu
 // Created: 02/25/2021
-// Updated: 10/26/2021
+// Updated: 04/06/2023
 //
 // See https://madmachine.io for more information
 //
@@ -23,7 +23,8 @@ public enum Id: Int32, IdName {
          D1, D2, D3, D4, D5, D6, D7, D8, D9, D10,
          D11, D12, D13, D14, D15, D16, D17, D18, D19, D20,
          D21, D22, D23, D24, D25, D26, D27, D28, D29, D30,
-         D31, D32, D33, D34, D35, RED, GREEN, BLUE
+         D31, D32, D33, D34, D35, D36, D37, D38, D39, D40,
+         D41, D42, D43, RED, GREEN, BLUE, DL
 
     case A0 = 0x0100, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13
 
@@ -39,9 +40,7 @@ public enum Id: Int32, IdName {
 
     case C0 = 0x0600, C1
 
-    case I2SOut0 = 0x0700
-    
-    case I2SIn0 = 0x0800
+    case I2S0 = 0x0700
 }
 
 #endif
